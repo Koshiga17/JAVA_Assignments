2639313ac64e79bfb01cf80e8d26a3c750299618
Koshiga17 <92245584+Koshiga17@users.noreply.github.com>
2025-01-02 08:10:06 -0800

    Create Variables_Example1.java

---
+++ Variables_Example1.java
@@ -0,0 +1,15 @@
+package com.cts;
+
+public class Variables_Example1 {
+
+	public static void main(String[] args) {
+		int number1=12;
+		int number2=13;
+		int number3=11;
+		System.out.println("sum of three numbers is: "+(number1+number2+number3));
+		System.out.println("average of three numbers is: "+(number1+number2+number3)/3);
+		System.out.println("sub of three numbers is: "+(number1-number2-number3));
+		System.out.println("multiplication of three numbers is: "+(number1*number2*number3));
+	}
+
+}
