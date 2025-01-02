21a52a284275893a69b5b9bb69921c16110bafe8
Koshiga17 <92245584+Koshiga17@users.noreply.github.com>
2025-01-02 08:09:24 -0800

    Create Variables_Example.java

---
+++ Variables_Example.java
@@ -0,0 +1,19 @@
+package com.cts;
+
+public class Variables_Example {
+
+	public static void main(String[] args) {
+		int number1=100;
+		int number2=100;
+		boolean result=number1==number2;
+		if(result!=true) {
+			System.out.println("number1 and number2 values are not equal");
+		}
+		else {
+			System.out.println("number1 and number2 values are equal");
+		}
+			
+
+	}
+
+}
